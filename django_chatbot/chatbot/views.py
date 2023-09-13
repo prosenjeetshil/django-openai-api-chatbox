4b53d902d6de166133917063508db1615ca7ba8c
prosenjeetshil <prosenjeetshil@gmail.com>
2023-09-13 15:19:08 +0530

    feat: integrated openai api

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from decouple import config
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = 'text-davinci-003',
+        prompt = message,
+        max_tokens = 150,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
 
 # Create your views here.
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'Hi this is my response'
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
